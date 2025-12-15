b51bf4068e917153ecc3415a69e14a5ed90dd708
Lulu <bouange45@gmail.com>
2025-12-15 17:35:33 +0000

    faites les we !!!!!

--- app/Models/product.php
+++ app/Models/product.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class product extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'quantity',
+    ];
 }
