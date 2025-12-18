a23f6954b557cdc3bdc7f131d2c7bad219c8dad8
kaiser1306 <cmohamedsayba@gmail.com>
2025-12-18 17:58:25 +0000

    Ajout de l'interface React complète avec CRUD produits, DTO utilisateurs, et configuration SPA

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Routes pour la gestion des produits
+Route::get('/products', [ProductController::class, 'apiIndex']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
+Route::put('/products/{product}', [ProductController::class, 'update']);
+Route::delete('/products/{product}', [ProductController::class, 'destroy']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+// Routes pour la gestion des utilisateurs
+Route::post('/users', [UserController::class, 'store']);
